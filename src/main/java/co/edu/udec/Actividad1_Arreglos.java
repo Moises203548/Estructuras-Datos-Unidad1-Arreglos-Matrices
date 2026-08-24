4975ce834e436be8b8d2b9b20e5087361acb3a0d
Moises203548 <mortegas4@unicartagena.edu.co>
2026-08-24 14:46:01 -0500

    feat(actividad1): declarar e inicializar arreglo con valores aleatorios

--- src/main/java/co/edu/udec/Actividad1_Arreglos.java
+++ src/main/java/co/edu/udec/Actividad1_Arreglos.java
@@ -1,8 +1,12 @@
 package co.edu.udec;
 
+import java.util.Random;
 public class Actividad1_Arreglos {
     public static void main(String[] args) {
-
-
+    int[] arreglo = new int[10];
+    Random random = new Random();
+    for (int i = 0; i < arreglo.length; i++){
+        arreglo[i] = random.nextInt(1000);
+    }
     }
 }
