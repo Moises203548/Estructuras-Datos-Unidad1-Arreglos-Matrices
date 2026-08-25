0e04d2c94f27c02fedd7695d5ce8db3f20345675
Moises203548 <mortegas4@unicartagena.edu.co>
2026-08-25 16:44:24 -0500

    feat(actividad2): imprimir matriz en forma de tabla

--- src/main/java/co/edu/udec/Actividad2_Matrices.java
+++ src/main/java/co/edu/udec/Actividad2_Matrices.java
@@ -7,5 +7,13 @@ public class Actividad2_Matrices {
                 {4, 5, 6},
                 {7, 8, 9}
         };
+
+        System.out.println("Matriz:");
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros[i].length; j++) {
+                System.out.print(numeros[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
