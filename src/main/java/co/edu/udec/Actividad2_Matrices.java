14df1649b9ae080538e7dd4bf5de9eaf659f5290
Moises203548 <mortegas4@unicartagena.edu.co>
2026-08-25 16:40:52 -0500

    feat(actividad2): declarar e inicializar matriz 3x3

--- src/main/java/co/edu/udec/Actividad2_Matrices.java
+++ src/main/java/co/edu/udec/Actividad2_Matrices.java
@@ -2,6 +2,10 @@ package co.edu.udec;
 
 public class Actividad2_Matrices {
     public static void main(String[] args) {
-
+        int[][] numeros = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
     }
 }
