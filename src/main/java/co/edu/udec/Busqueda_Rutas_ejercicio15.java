d39a7226b96f776f76b456f8fc05e994df71befa
Moises203548 <mortegas4@unicartagena.edu.co>
2026-09-03 11:22:34 -0500

    feat(actividad3): inicializar estructuras de datos para BFS

--- src/main/java/co/edu/udec/Busqueda_Rutas_ejercicio15.java
+++ src/main/java/co/edu/udec/Busqueda_Rutas_ejercicio15.java
@@ -1,5 +1,8 @@
 package co.edu.udec;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Busqueda_Rutas_ejercicio15 {
     static int numeroFilas, numeroColumnas;
     static char[][] mapaLaberinto;
@@ -15,5 +18,21 @@ public class Busqueda_Rutas_ejercicio15 {
         numeroFilas = mapaLaberinto.length;
         numeroColumnas = mapaLaberinto[0].length;
 
+        int filaInicio = 0, columnaInicio = 0;
+        int filaSalida = 4, columnaSalida = 4;
+
+        boolean[][] celdaVisitada = new boolean[numeroFilas][numeroColumnas];
+
+        int[][] filaOrigen = new int[numeroFilas][numeroColumnas];
+        int[][] columnaOrigen = new int[numeroFilas][numeroColumnas];
+
+        Queue<int[]> celdasPorExplorar = new LinkedList<>();
+        celdasPorExplorar.add(new int[]{filaInicio, columnaInicio});
+        celdaVisitada[filaInicio][columnaInicio] = true;
+
+        int[] movimientoFila   = {-1,  1,  0,  0};
+        int[] movimientoColumna = { 0,  0, -1,  1};
+
+        boolean rutaEncontrada = false;
     }
 }
