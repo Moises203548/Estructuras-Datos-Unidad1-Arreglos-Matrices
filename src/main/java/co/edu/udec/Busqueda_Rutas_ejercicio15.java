ccbea0e05d3e59b9932d7be02132e3232aa3c769
Moises203548 <mortegas4@unicartagena.edu.co>
2026-09-03 11:25:28 -0500

    feat(actividad3): implementar algoritmo de busqueda BFS

--- src/main/java/co/edu/udec/Busqueda_Rutas_ejercicio15.java
+++ src/main/java/co/edu/udec/Busqueda_Rutas_ejercicio15.java
@@ -34,5 +34,32 @@ public class Busqueda_Rutas_ejercicio15 {
         int[] movimientoColumna = { 0,  0, -1,  1};
 
         boolean rutaEncontrada = false;
+
+        while (!celdasPorExplorar.isEmpty()) {
+            int[] celdaActual = celdasPorExplorar.poll();
+            int filaActual = celdaActual[0];
+            int columnaActual = celdaActual[1];
+
+            if (mapaLaberinto[filaActual][columnaActual] == 'E') {
+                rutaEncontrada = true;
+                break;
+            }
+
+            for (int direccion = 0; direccion < 4; direccion++) {
+                int filaVecina = filaActual + movimientoFila[direccion];
+                int columnaVecina = columnaActual + movimientoColumna[direccion];
+
+                boolean estaDentroDelMapa = filaVecina >= 0 && filaVecina < numeroFilas && columnaVecina >= 0 && columnaVecina < numeroColumnas;
+
+                boolean esCeldaValida = estaDentroDelMapa && !celdaVisitada[filaVecina][columnaVecina] && mapaLaberinto[filaVecina][columnaVecina] != '#';
+
+                if (esCeldaValida) {
+                    celdaVisitada[filaVecina][columnaVecina] = true;
+                    filaOrigen[filaVecina][columnaVecina] = filaActual;
+                    columnaOrigen[filaVecina][columnaVecina] = columnaActual;
+                    celdasPorExplorar.add(new int[]{filaVecina, columnaVecina});
+                }
+            }
+        }
     }
 }
