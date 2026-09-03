3e43c962330c00d5a4f5bc79fc5918cfffae2186
Moises203548 <mortegas4@unicartagena.edu.co>
2026-09-03 11:18:36 -0500

    feat(actividad3): crear mapa con matriz

--- src/main/java/co/edu/udec/Busqueda_Rutas_ejercicio15.java
+++ src/main/java/co/edu/udec/Busqueda_Rutas_ejercicio15.java
@@ -1,8 +1,19 @@
 package co.edu.udec;
 
 public class Busqueda_Rutas_ejercicio15 {
+    static int numeroFilas, numeroColumnas;
+    static char[][] mapaLaberinto;
     public static void main(String[] args) {
+        mapaLaberinto = new char[][]{
+                {'S', '.', '#', '.', '.'},
+                {'#', '.', '#', '.', '#'},
+                {'.', '.', '.', '.', '.'},
+                {'.', '#', '#', '#', '.'},
+                {'.', '.', '.', '#', 'E'}
+        };
 
+        numeroFilas = mapaLaberinto.length;
+        numeroColumnas = mapaLaberinto[0].length;
 
     }
 }
